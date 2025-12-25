65a5a5321c7ea1a3beaaf39d36b1eb53209766eb
Sukhrobzhon Talibzhanov <tolibjanov05@gmail.com>
2025-12-25 18:45:58 +0500

    Order section half is done

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Results/AddressResults/GetAddressByIdQueryResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Results.AddressResults
+{
+    public class GetAddressByIdQueryResult
+    {
+        public int AddressId { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public string District { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Detail { get; set; } = string.Empty;
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Domain/Entities/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Domain.Entities
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public string District { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Detail { get; set; } = string.Empty;
+    }
+}
